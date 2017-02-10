1a59511e1decbde56b9ac66ae13641e92dd236c3
Tony_Silva <tonysilva.ou@gmail.com>
2017-02-10 09:14:32 -0600

    fixed capitalization issues and started using a dictionary

--- edit_distance.py
+++ edit_distance.py
@@ -1,21 +1,36 @@
+# Edit Distance Assignment
+# Tony Silva CS 5970
+
+
 def distance(b, a):
-    a = a.lower()
-    b = b.lower()
+    # I do this just so I know I am consistent in my function and
+    # the actions being performed.
     if len(b) < len(a):
         return distance(a, b)
+    # Obviously, if a is empty we return the size of the
+    # non empty string
     if len(a) == 0:
         return len(b)
     # declare size of list of lists
-    d = [[0 for x in range(len(a))] for y in range(len(b))]
-    for i in range(len(b)):
-        d[i][0] = i
-    for j in range(len(a)):
-        d[0][j] = j
+    m = len(b)+1
+    n = len(a)+1
+    # easier to implement with a list, in my opinion
+    d = {}
+    # initialize the list from 0 to m and n
+    # this way if the word is cat the size of d will be from
+    # 0, 1 , 2, 3
+    for i in range(m):
+        d[i, 0] = i
+    for j in range(n):
+        d[0, j] = j
     # create distance matrix numbers
-    for i in range(len(b)):
-        for j in range(len(a)):
-            if b[i] == a[j]:
-                d[i][j] = d[i-1][j-1]
+    for i in range(1, m):
+        for j in range(1, n):
+            # if the lets are not equal then we need to add the minimum of the costs
+            if b[i-1] == a[j-1]:
+                d[i, j] = d[i-1, j-1]
             else:
-                d[i][j] = min(d[i-1][j] + 1, d[i][j-1] + 1, d[i-1][j-1] + 1)
-    return d[-1][-1]
+                d[i, j] = min(d[i-1, j] + 1, d[i, j-1] + 1, d[i-1, j-1] + 1)
+    # return the last entry of the dictionary
+    return d[m-1, n-1]
+
